b8ea52a2c462271071dbe0e81696f41dc7b79781
Bryan <bryanurubia@gmail.com>
2019-03-31 22:07:05 -0600

    more changes

--- TheProject/src/app/recipes/recipes-list/recipes-list.component.ts
+++ TheProject/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe} from '../recipe.model'
+
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
@@ -7,6 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesListComponent implements OnInit {
 
+  recipes : Recipe[] = [
+    new Recipe("Apa","Dad's best APA",
+    'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'),
+    new Recipe("Apa","Dad's best APA",
+    'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'),
+    new Recipe("Apa","Dad's best APA",
+    'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'),
+    new Recipe("Apa","Dad's best APA",
+    'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')
+  ];
   constructor() { }
 
   ngOnInit() {
--- TheProject/src/app/shoping-list/shoping-list.component.ts
+++ TheProject/src/app/shoping-list/shoping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shoping-list',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopingListComponent implements OnInit {
 
+  ingredients : Ingredient[] = [
+    new Ingredient("apple",5),
+    new Ingredient("apple- bryan",5)
+  ];
   constructor() { }
 
   ngOnInit() {
