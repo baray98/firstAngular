8cdf41ec1c3947156814f980d1d44103d525ec13
Bryan <bryanurubia@gmail.com>
2019-04-03 20:21:11 -0600

    Added bindings

---
+++ databinding-bry/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // title = 'databinding-bry';
+  serverElements = [];
+  @Input() newServerName  = '';
+  @Input() newServerContent = '';
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+}
